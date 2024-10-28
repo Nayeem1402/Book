f3a9bce736452c785859fdb044689da48fb0fbcd
Nayeem1402 <mnmdnayeem@gmail.com>
2024-10-29 02:26:37 +0600

    main banner compleat

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import MainBanner from "../MainBanner/MainBanner";
 import Navbar from "../NavBar/Navbar";
 
  const Home = () => {
     return (
          <>
          <Navbar></Navbar>
+         <MainBanner></MainBanner>
          </>
     );
 };
---
+++ src/Components/MainBanner/MainBanner.jsx
@@ -0,0 +1,14 @@
+const MainBanner = () => {
+    return (
+        <div className="mx-32 h-[554px] bg-[#1313130D] rounded-xl mt-12 flex items-center justify-around">
+            <div className="">
+                <h1 className="font-bold mb-12 text-6xl">Books to freshen <br /> up your bookshelf</h1>
+
+                <button className="g-btn">View The List</button>
+            </div>
+           <img className="" src="./6.jpg" alt=" " />
+        </div>
+    );
+};
+
+export default MainBanner;
